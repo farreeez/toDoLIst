f55b6d40c09360a0b7ab91fce1b1f2b22cd37708
farreeez <fares.selwadii@gmail.com>
2025-11-29 17:30:57 +1300

    made clicking checkboxes easier for the user

--- frontend/src/pages/home-page/components/TodoItem.tsx
+++ frontend/src/pages/home-page/components/TodoItem.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import type { TodoItem as TodoItemModel } from "../../../types/TodoItem";
 import styles from ".././HomePage.module.css";
 
 export const TodoItem = ({ item }: { item: TodoItemModel }) => {
+  const [isDone, setIsDone] = useState(false);
   return (
-    <div className={styles.todoContainer}>
-      <input type="checkbox" />
+    <div
+      className={styles.todoContainer}
+      onClick={() => {
+        setIsDone((prev) => !prev);
+      }}
+    >
+      <input className={styles.checkBox} type="checkbox" checked={isDone} />
       <div className={styles.todoDetails}>
         <div className={styles.todoName}>{item.name}</div>
         <div className={styles.todoDate}>
